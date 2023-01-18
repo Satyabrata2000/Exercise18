dc61e12446a6092adb5f05e562401e374504effa
Satyabrata2000 <satyabrata2000@gmail.com>
2023-01-18 09:33:21 -0600

    Exercise18

---
+++ Example1.java
@@ -0,0 +1,12 @@
+package Exercise18;
+
+//Calling static method in java
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        int a;
+        a = Math.min(23, 36); //calling static method of the Math class
+        System.out.println("Minimum no is " + a);
+    }
+}
---
+++ Example2.java
@@ -0,0 +1,13 @@
+package Exercise18;
+
+//Calling the predefined method
+public class Example2 {
+
+    public static void main(String[] args) {
+
+        int a;
+        Object obj = new Object();
+        a = obj.hashCode();
+        System.out.println("Hash Code of the object is " + a);
+    }
+}
